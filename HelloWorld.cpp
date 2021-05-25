6f83d55aecdbee355f390b6d2cd6f5faea57f5fb
Sadeem Shamoun <sadeem23.sj@gmail.com>
2021-05-25 18:46:29 -0400

    First commit for assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,20 @@
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+
+//Adding comments for fun and conflict
+//Version of master branch
+
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello world"; 
+      
+    return 0; 
+} 
